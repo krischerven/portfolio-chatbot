04fe9432bd044d7b69fe7867479a20a2259d12ad
Kris Cherven <krischerven@gmail.com>
2023-09-06 11:45:51 -0400

    Various improvements

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module git.krischerven.info/resume-chatbot
 
 go 1.19
 
-require github.com/sashabaranov/go-openai v1.15.1
+require (
+	github.com/sashabaranov/go-openai v1.15.1
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	openai "github.com/sashabaranov/go-openai"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"os"
+	"os/exec"
+	"strings"
 )
+
+const instructions = `The following is information about a software engineer named Kris Cherven. There is a resume section and a facts section.
+Information in the facts section must take priority over information in the resume section. The resume section starts after the text
+"BEGINNING OF RESUME SECTION" and ends after the text "END OF RESUME SECTION". The facts section starts after the text "BEGINNING OF FACTS
+SECTION" and ends after the text "END OF FACTS SECTION". After the facts section you will be asked a question. When answering questions about
+the school Kris Cherven went to, keep in mind that you should consider Grand Circus Java Bootcamp to be his main school. Also, do not mention
+the "facts section" or the "resume section" when answering questions. These are internal sections that are of no interest to the person
+asking the question. Never use phrases like "based on the information provided."; instead, act as if you are a human being who knows all of
+the information you are being asked for.`
+
+var facts = []string{
+	// <2023-09-06 Wed> GPT-3 thinks its October 2022 unless you tell it otherwise
+	"The current date is September 6, 2023.",
+	"Kris Cherven is 24 years old.",
+}
 
 func fail(err error) {
 	if err != nil {
@@ -14,33 +32,33 @@ func fail(err error) {
 	}
 }
 
-func answerQuestion(question string) string {
-	str, err := os.ReadFile("API_KEY")
+func readFile(name string) string {
+	bs, err := os.ReadFile(name)
 	fail(err)
+	return string(bs)
+}
 
-	apiKey := string(str)
+func initializeClient() *openai.Client {
+
+	apiKey := readFile("API_KEY")
 	if apiKey == "" {
-		log.Fatalln("Missing API key")
+		log.Fatal("Missing API key")
 	}
 
-	str, err = os.ReadFile("instructions.txt")
-	fail(err)
+	return openai.NewClient(apiKey)
+}
 
-	instructions := string(str)
-	if instructions == "" {
-		log.Fatalln("Missing instructions")
-	}
-
-	client := openai.NewClient(apiKey)
+func answerQuestion(question string, information string, client *openai.Client) string {
 
 	// https://pkg.go.dev/github.com/PullRequestInc/go-gpt3#CompletionRequest
 	resp, err := client.CreateChatCompletion(context.Background(),
 		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
+			Model:     openai.GPT3Dot5Turbo,
+			MaxTokens: 200,
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    openai.ChatMessageRoleUser,
-					Content: instructions + "\n\n" + question,
+					Content: information + "\n" + question,
 				},
 			},
 		})
@@ -50,12 +68,22 @@ func answerQuestion(question string) string {
 }
 
 func main() {
-	for {
-		var question string
-		_, err := fmt.Scanln(&question)
-		if err != nil {
-			continue
-		}
-		fmt.Println(answerQuestion(question))
+
+	outFile := "instructions.txt"
+	{
+		fail(exec.Command("pdftotext", "resume.pdf").Run())
+		fail(exec.Command("mv", "resume.txt", outFile).Run())
+		text := readFile(outFile)
+		text = strings.Replace(instructions, "\n", " ", -1) + "\n\nBEGINNING OF RESUME SECTION\n\n" + text + "\n\nEND OF RESUME SECTION\n\n"
+		text = text + "BEGINNING OF FACTS SECTION\n\n" + strings.Join(facts, "\n") + "\n\nEND OF FACTS SECTION\n"
+		os.WriteFile(outFile, []byte(text), 0644)
+	}
+	information := readFile(outFile)
+
+	fmt.Println("Hello! I am resume-chatbot. Please go ahead and ask me any questions you have about Kris!")
+	scanner := bufio.NewScanner(os.Stdin)
+	client := initializeClient()
+	for scanner.Scan() {
+		fmt.Println(answerQuestion(scanner.Text(), information, client))
 	}
 }
