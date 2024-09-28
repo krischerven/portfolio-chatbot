692b96241eb48d28cdc4c0c8fe71a051e510acf3
Kris Cherven <krischerven@gmail.com>
2024-09-28 13:32:46 -0400

    Remove old code snippet comment

--- main.go
+++ main.go
@@ -1,8 +1,3 @@
-/* *****************************************************************
-   * main.go: The entire source code for the chatbot on this page. *
-   * https://git.krischerven.info/dev/portfolio-chatbot            *
-   ***************************************************************** */
-
 package main
 
 import (
